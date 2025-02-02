d90edf9d7c59b3acb4a6c3922149e6590b1f0621
Brad Kittrell <bradkittrell@gmail.com>
2025-02-01 23:51:13 -0500

    simple changes

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -1,6 +1,18 @@
-def load_names():
+import polars as pl
+
+
+def load_names(path: str) -> pl.DataFrame:
     """
     Load the names dataset from the processed data folder.
 
     """
-    return pl.read_parquet('data/processed/names.parquet')
+
+    if path:
+        return pl.read_parquet(path)
+    else:
+        # use static path
+        path = "/Users/bradkittrell/Projects/imdb/IMDB_Fun/Data/processed/name.parquet"
+
+    names = pl.read_parquet(path)
+    names = names.filter(pl.col("birthYear") > 1899)
+    return names
